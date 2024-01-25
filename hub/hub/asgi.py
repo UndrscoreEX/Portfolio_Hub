0e9993bb082c5e9096425907eed0d0e3eefb9355
UndrscoreEX <ethan.heikell.official@gmail.com>
2024-01-25 09:10:42 +0900

    indent

--- hub/hub/asgi.py
+++ hub/hub/asgi.py
@@ -23,7 +23,7 @@ application = ProtocolTypeRouter({
 })  
 
 if __name__ == "__main__":
-   server = Server(application)
+    server = Server(application)
     # server.listen('0.0.0.0',2020)
 #    server.run(port=8000)
     server.run(application, host='0.0.0.0', port=8000)
