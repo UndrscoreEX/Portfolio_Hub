cc83a2f5a5492ba8f2e387d83f49f5cad1221816
UndrscoreEX <ethan.heikell.official@gmail.com>
2024-09-07 14:41:19 +0900

    adding wait before the validation. I think its checking too quickly

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # simple check . one day ill make a proper check endpoint
+echo "Waiting for 60 seconds to let the service stabilize..."
+sleep 60
+
 if curl -s http://localhost:8000 | grep "Ethan"; then
 	  echo "App is running successfully"
 	    exit 0
